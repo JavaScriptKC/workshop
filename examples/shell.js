91791d7494f43ee99b9c0663842202fee1d39cbc
Joe Andaverde <joe@andaverde.net>
2012-09-29 22:15:33 -0500

    Implemented shell... This may be too complicated for a beginner crowd

--- examples/shell.js
+++ examples/shell.js
@@ -17,14 +17,55 @@ var commands = {
       process.chdir(path.resolve(process.cwd(), args[0]));
    },
    'tail': function (args) {
+      fs.stat(args[0], function (err, stats) {
+         
+         var streamOptions = { 
+           flags: 'r',
+           encoding: 'utf8',
+           mode: 0666,
+           bufferSize: stats.blksize,
+           start: 0,
+           end: stats.size
+         };
 
+         var offset = 0;
+         var numLines = 1;
+         var newLines = new Array(numLines);
+         var index = 0;
+
+         var fileStream = fs.createReadStream(args[0], streamOptions);
+         
+         fileStream.on('data', function (data) {
+            for (var i = 0; i < data.length; i++) { 
+               if (data[i] === '\n') {
+                  newLines[index] = (offset * stats.blksize) + i;
+                  index = ++index % numLines;
+               }
+            }
+
+            offset++;
+         });
+
+         fileStream.on('end', function () {
+            var end = newLines.splice(0, index);
+            newLines = newLines.concat(end);
+
+            streamOptions.start = newLines[0] + 1;
+
+            var stream = fs.createReadStream(args[0], streamOptions);
+
+            stream.on('data', function (d) {
+               console.log(d.toString());
+            });
+         });
+      });  
    }
 };
 
 stdin.on('data', function (d) {
    var matches = d.toString().match(/(\w+)(.*)/i);
    var command = matches[1].toLowerCase();
-   var args = matches[2].trim().split(/\s/);
+   var args = matches[2].trim().split(/\s+/);
 
    commands[command](args);
 });

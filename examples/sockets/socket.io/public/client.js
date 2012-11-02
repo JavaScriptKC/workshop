44bd5aa8325252e0ce2292d42890e1653a08a95a
Russell Madsen <ruzz311@gmail.com>
2012-11-02 03:14:51 -0500

    client api updates - "User" api now IIFE with common function for most emitters

--- examples/sockets/socket.io/public/client.js
+++ examples/sockets/socket.io/public/client.js
@@ -5,81 +5,76 @@ var socket = io.connect('http://localhost:3000'),
 
 //==================== User resource emitters (events / data sent to the server)
 
-  User = {
-    create : function(user){
-      socket.emit('user.create', user, function(err, res){
-        if(err)
-          console.log('user create error', err, res);
-      });
-    },
+  User = (function(){
     
-    read : function(id, cb){
-      socket.emit('user.read', id, function(err, res){
-        if(err)
-          console.log(err)
-        else if(id) 
-          collection[id] = res
-        else 
-          collection = res
+    var _common = function (action, user, cb) {
+      socket.emit(action, user, function (err, res) {
+        if (err)
+          console.log(err, action, res);
         
-        if(typeof cb === "function")
+        if (typeof cb === "function") 
           cb(res);
       });
-    },
+    };
     
-    update : function(user){
-      socket.emit('user.update', user, function(err, res){
-        if(err)
-          console.log('user.update error', err, res);
-      });
-    },
-    
-    destroy : function(user){
-      socket.emit('user.destroy', user, function(err, res){
-        if(err)
-          console.log('user.destroy error', err, res);
-      });
+    return {
+      read : function (id, cb) {
+        socket.emit('user.read', id, function(err, res){
+          if (err){
+            console.log(err, res);
+            return false;
+          }
+          
+          if (id)
+            collection[id] = res
+          else
+            collection = res;
+          
+          if (typeof cb === "function") cb(res);
+        });
+      },
+      create  : function (user, cb) { _common('user.create', user, cb) },
+      update  : function (user, cb) { _common('user.update', user, cb) },
+      destroy : function (user, cb) { _common('user.destroy', user, cb) }
     }
-  };
+  })();
 
 
 //==================== User resource listeners (listen for socket server events)
 
-  // Socket events from the server
+  // Socket connection events from the server
+  socket.on('connection.me', function(){ User.read(null, updateView) });
   socket.on('connection.join', function () { console.log("Connection.join") });
   socket.on('connection.drop', function () { console.log("Connection.drop") });
 
   // User resource listeners (events emitted from the server)
   socket.on('user.created', function (user) {
-    console.log('user.created', user);
     collection[user.github] = user;
     updateView();
   });
 
   socket.on('user.updated', function (user) {
-    console.log('user.updated', user);
     collection[user.github] = user;
     updateView();
   });
 
-  socket.on('user.destroyed', function (user) {
-    console.log('user.destroyed', user);
-    delete collection[user.github];
+  socket.on('user.destroyed', function (userid) {
+    delete collection[userid];
     updateView();
   });
-  
+
 
 //========================================================================= Init
   
-  User.read(null, function(d){ 
-    console.log('List of users obtained!', d) 
-    updateView();
-  });
+
   /*
-  User.read(null, function(d){ console.log('sweet', d) });
-  User.read('ruzz311', function(d){ console.log('double sweet', d) });
+  User.read('ruzz311', function(d){ console.log('yay', d) });
   User.create({ 
-    "github": Math.floor((Math.random()*999)+1), 
-    "name": "fake user" 
-  })
+    "github": "repbl4lyfe", 
+    "name": "mitt romney" 
+  });
+  User.update({ 
+    "github": "ruzz311", 
+    "name": "russell madsen" 
+  });
   */

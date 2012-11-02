d143c94f815eaf553821114bcf0345a407781589
Russell Madsen <ruzz311@gmail.com>
2012-11-02 01:11:13 -0500

    simplify / clean up of server-side socket RPCs

--- examples/sockets/socket.io/app.js
+++ examples/sockets/socket.io/app.js
@@ -1,76 +1,84 @@
 
 //================================================================= Dependencies
 
-  var express = require('express'), 
-      io = require('socket.io'), 
+  var express = require('express'),
+      io = require('socket.io'),
       http = require('http'),
       path = require('path'),
-      users = [
-        { name: 'joseph' }, 
-        { name: 'mike' }, 
-        { name: 'dusty' }, 
-        { name: 'russell' }, 
-        { name: 'joe' }, 
-        { name: 'andrew' }
-      ],
-      app, server, io;
-
-  app = express();
-  server = http.createServer(app);
-
+      app, server, io, users;
+      
 
 //================================================================ Express Setup
-  app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.directory(path.join(__dirname, 'public')));
-  });
 
-  app.configure('development', function(){
-    app.use(express.errorHandler());
-  });
+  app = express();
+  app.set("port", process.env.PORT || 3000);
+  app.use(express.static( path.join(__dirname, "public") ));
   
-  server.listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-  });
+  server = http.createServer(app);
+  server.listen(app.get("port"));
 
 
-//================================================================= Socket Start
+//=============================================================== Socket RPC API
+  users = {
+    "joeandaverde": { name: "joseph",   github: "joeandaverde" }, 
+    "mhemesath":    { name: "mike",     github: "mhemesath" }, 
+    "dustyburwell": { name: "dusty",    github: "dustyburwell" }, 
+    "ruzz311":      { name: "russell",  github: "ruzz311" }, 
+    "joelongstreet":{ name: "joe",      github: "joelongstreet" }, 
+    "adunkman":     { name: "andrew",   github: "adunkman" }
+  };
   
   io = io.listen(server);
   
   io.sockets.on('connection', function (client) {
     
-    // resource event listeners
+    // resource create
     client.on('user.create', function (user, cb) {
-      users.push(user);
-      cb(null, user);
-      io.sockets.emit('user.created', { data: user });
+      if (users[user.github]) {
+        cb("ERROR: user already exists", null);
+      } else {
+        users[user.github] = user;
+        cb(null, user);
+        io.sockets.emit('user.created', user);
+      }
     });
     
+    // resource read
     client.on('user.read', function (id, cb) {
-      if(id){
-        console.log("id===null", id);
-        cb(null, users[id]);
+      if (id){
+        err = (users[id]) ? null : "ERROR: no user found with id = "+id;
+        cb(err, users[id]);
       }
       cb(null, users);
     });
     
+    // resource update
     client.on('user.update', function (user, cb) {
-      users[user.id] = user;
+      if (typeof users[user.github] === "undefined") {
+        users[user.github] = user;
+      }
       cb(null, user);
-      client.broadcast.emit('user.updated', { data: user });
+      io.sockets.emit('user.updated', user);
     });
     
-    client.on('user.destroy', function (user, cb) {
-      cb(null, user);
-      client.broadcast.emit('user.deleted', { data: user });
+    // resource destroy
+    client.on('user.destroy', function (id, cb) {
+      if (typeof users[id] === "undefined") {
+        
+      } else {
+        
+      }
+      var deletedUser = users[id];
+      delete users[id];
+      cb(null, deletedUser);
+      io.sockets.emit('user.destroyed', deletedUser);
     });
     
     // system event emitters/listeners
     io.sockets.emit('connection.join');
     
     client.on('disconnect', function () { 
-      client.broadcast.emit('connection.drop', { client: client });
+      io.sockets.emit('connection.drop');
     });
+    
   });

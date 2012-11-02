e556222c7d0ab2a4938714f4e2bee97110cd2fd9
Russell Madsen <ruzz311@gmail.com>
2012-11-02 05:02:58 -0500

    nit-picking a couple of things in the tcp-socket example

--- examples/sockets/tcp-socket.js
+++ examples/sockets/tcp-socket.js
@@ -3,20 +3,20 @@ var net = require('net'), server, s;
 // socket events
 socket = {
   
-  // server start event
+  // server start
   onStart: function(){ 
     console.log('server started') 
   },
   
-  // handle new connections
+  // handle new connection
   onClientConnect: function(conn){
-    console.log('connected', conn);
     conn.on('end', socket.onClientEnd);
     conn.on('data', socket.onClientData);
     conn.write('client connected to server\r\n');
     conn.pipe(conn);
   },
   
+  // handle data
   onClientData: function(buf){ 
     var str   = buf.toString('utf8'),
         bytes = Buffer.byteLength(str, 'utf8'),
@@ -26,22 +26,22 @@ socket = {
     console.log('client -> server message: "%s"', str);
   },
   
-  // client disconnect event
+  // client disconnect
   onClientEnd: function(){ 
     console.log('disconnected') 
   }
+  
 };
 
-// Create & Start 
+// Create and Start 
 server = net.createServer(socket.onClientConnect);
 server.listen(3333, socket.onStart);
 
-/** (assumed *nix based OS)
-  *
+/**
   * Start socket server 
-  *   -> $ node sockets-1.0.js)
+  *   -> $ node tcp-socket.js
   *
-  * telnet into your server you just started.  you should see "What up NodeLabs KC?"
+  * telnet into your server you just started.  you should see "client connected to server"
   *   -> $ telnet 127.0.0.1 3333
   *
   * At this point you are in a telnet session with your TCP server.

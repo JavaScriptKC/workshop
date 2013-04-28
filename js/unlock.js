5596f0f2dba18bf8dd90be3c12958fc8978500bd
Andrew Dunkman <andrew@dunkman.org>
2013-04-28 18:52:10 -0500

    Forgot to remove localhost, whoops

--- js/unlock.js
+++ js/unlock.js
@@ -1,7 +1,7 @@
 (function () {
 
-  //var socket = io.connect('http://nodelabs.herokuapp.com:80');
-  var socket = io.connect('http://localhost:4040');
+  var socket = io.connect('http://nodelabs.herokuapp.com:80');
+  //var socket = io.connect('http://localhost:4040');
 
   socket.on("connect", function () {
     socket.emit("username", whoami());
